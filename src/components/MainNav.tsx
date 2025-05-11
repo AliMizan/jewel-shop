5b61035f5c7ee82fe3eaa8dab906506c9a5f6dc2
Mizan <mizanali097@gmail.com>
2025-05-11 23:36:24 +0530

    cart-icon

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -111,7 +111,7 @@ const MainNav = () => {
                 <ShoppingCart  />
 
       
-        {  (
+        { cart.length > 0 && (
         <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
           {cart.length}
         </span>
